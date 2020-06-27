46adc1e27389cbb1e94412e781d2d4b5dfd6f9ec
Happiest-d <31284435+Happiest-d@users.noreply.github.com>
2020-06-27 23:32:26 +0300

    syntax fixes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
-const prs = require('body-parser')
+const bodyParser = require('body-parser')
 //const request = require('request')
 const fetch = require('node-fetch')
 
 const app = express()
 
-let reqTime = {
+const reqTime = {
     last: 0,
     sumTime: 0,
     sumReq: 0,
@@ -14,46 +14,40 @@ let reqTime = {
     periodReq: 0
 }
 
-app.use(prs.json())
+app.use(bodyParser.json())
 
-app.post('/api/comments', (req, res) => {
+app.post('/api/comments', async(req, res) => {
     //const dataInput = req.body;
-    async function getData() {
-        const start = new Date().getTime()
-        const link = 'https://jsonplaceholder.typicode.com/comments'
-        const response = await fetch(link)
-        const comments = await response.json()
-    
-        //Поиск автора максимального кол-ва комментариев
-        let email = comments.map(comment => comment.email)
-        const uniqMail = findAllUniq(email)
-        uniqMail.sort(sortDescending)
-        
-    
-        //Поиск 5 наиболее частых слов
-        let allWords = []
-        comments.forEach(comment => {
-            const words = findWords(comment.body)
-            words.forEach(word => allWords.push(word))
-        })
-        let uniqWords = findAllUniq(allWords)
-        uniqWords.sort(sortDescending)
-    
-    
-        //вывод результата
-        const end = new Date().getTime()
-        timeFind(start, end)
-        //console.log(data)
-        const resData = {
-            author: uniqMail[0],
-            words: uniqWords.slice(0,5),
-            time: reqTime
-        }
-        res.json(resData)
-    }
+    const start = new Date().getTime()
+    const link = 'https://jsonplaceholder.typicode.com/comments'
+    const response = await fetch(link)
+    const comments = await response.json()
 
-    getData()
+    //Поиск автора максимального кол-ва комментариев
+    let email = comments.map(comment => comment.email)
+    const uniqMail = findAllUniq(email)
+    uniqMail.sort(sortDescending)
     
+
+    //Поиск 5 наиболее частых слов
+    let allWords = []
+    comments.forEach(comment => {
+        const words = findWords(comment.body)
+        words.forEach(word => allWords.push(word))
+    })
+    let uniqWords = findAllUniq(allWords).sort(sortDescending)
+
+
+    //вывод результата
+    const end = new Date().getTime()
+    timeFind(start, end)
+    //console.log(data)
+    const resData = {
+        author: uniqMail[0],
+        words: uniqWords.slice(0,5),
+        time: reqTime
+    }
+    res.json(resData)
 })
 
 
@@ -62,7 +56,7 @@ const timeFind = (start, end) => {
     reqTime.sumTime += reqTime.last
     reqTime.sumReq++
     reqTime.avgTime = reqTime.sumTime / reqTime.sumReq
-    reqTime.allReqTime[reqTime.sumReq-1] = end
+    reqTime.allReqTime[reqTime.sumReq - 1] = end
     //кол-во запросов за поледнюю минуту
     reqTime.periodReq = 0
     for (let i = 0; i < (reqTime.sumReq); i++) {
@@ -72,18 +66,18 @@ const timeFind = (start, end) => {
 
 const sortDescending = (a, b) => {
     if (a.counter < b.counter) {
-      return 1;
+      return 1
     }
     if (a.counter > b.counter) {
-      return -1;
+      return -1
     }
-    return 0;
+    return 0
 }
 
-const findAllUniq = (arrIn) => {
+const findAllUniq = (inputArray) => {
     let arrOut = []
-    arrIn.forEach(element => {
-        const wordIndex = arrOut.findIndex(uniqElement => element == uniqElement.arg)
+    inputArray.forEach(element => {
+        const wordIndex = arrOut.findIndex(uniqElement => element === uniqElement.arg)
         if (wordIndex >= 0) {
             arrOut[wordIndex].counter++
         } else {
@@ -106,7 +100,8 @@ const findAllUniq = (arrIn) => {
 const findWords = (string) => {
     const wordsFilter = /[^a-zA-Z_]/
     const splitString = string.split(wordsFilter)
-    const words = splitString.filter(word => word != '' )
+    const words = splitString.filter(word => word !== '')
+
     return words
 }
 
